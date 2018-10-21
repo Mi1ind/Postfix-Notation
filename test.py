e7a96fa8df5e5669719dc504dc5bbdf7120d621e
Milind Sai Waghrey <m.sai.waghrey@gmail.com>
2018-10-20 20:22:56 -0400

    input split into multiple variables

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-prob = raw_input("Display the problem in postfix notation? ")
-print ("The problem you displayed is"), prob
+# prob = raw_input("Display the problem in postfix notation? ")
+# print ("The problem you displayed is"), prob
 
+prob = raw_input("Enter first 3 coordinates (example: 102 36 74): ")
+
+tokens = prob.split() # returns a list (ArrayList) of tokenized values
+coords = []     # initialise empty list
+for tkn in tokens:
+    try:
+        coords.append(int(tkn))     # convert token to int
+    except ValueError:       # if the entered number was of invalid int format
+        print("Invalid integer format: {}".format(tkn))
+        raise
+print(coords)   # coords is now a list of integers that were entered
