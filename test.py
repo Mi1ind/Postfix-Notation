9fc38f422e1f251a76f12ca6d2441fe64de86480
Milind Sai Waghrey <m.sai.waghrey@gmail.com>
2018-10-20 20:54:04 -0400

    can identify if input is a digit (does not work for negative numbers)

--- test.py
+++ test.py
@@ -7,8 +7,15 @@ tokens = prob.split() # returns a list (ArrayList) of tokenized values
 coords = []     # initialise empty list
 for tkn in tokens:
     try:
-        coords.append(int(tkn))     # convert token to int
+        coords.append(tkn)     # convert token to string
     except ValueError:       # if the entered number was of invalid int format
         print("Invalid integer format: {}".format(tkn))
         raise
 print(coords)   # coords is now a list of integers that were entered
+
+
+if coords[1].isdigit():
+    print("IT'S A NUMBER")
+else:
+    print("NOT A number")
+     
